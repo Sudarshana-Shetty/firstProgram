5717f9c3c4188a46f8d332d50fc1a65e84503915
Sudarshana-Shetty <48707855+Sudarshana-Shetty@users.noreply.github.com>
2019-04-29 10:17:27 +0530

    Add files via upload

---
+++ AllFunctions.py
@@ -0,0 +1,64 @@
+class SubClass:
+    def __init__(self, db):
+        self.db = db
+
+# >>>>>> --- Accepting user name and address here --- <<<<<<
+    def userInput(self):
+        userInput = {}
+        uname = str(input("Enter User Name: "))
+        uaddress = str(input('Enter user address: '))
+        userInput['Name'] = uname
+        userInput['Address'] = uaddress
+        return userInput
+
+# >>>>>> --- Checking for user if exists --- <<<<<<                    
+    def findUser(self, uInput):
+        uCollections = self.db.UserCollections.find({'Name': uInput['Name']})
+        for uInput in uCollections:
+            print(uInput)
+            return 1 
+
+# >>>>>> --- Display one user details --- <<<<<<
+    def displayOneUser(self, uInput):
+        uCollections = self.db.UserCollections.find({'Name': uInput['Name']})
+        for uInput in uCollections:
+            print(uInput)
+            return uInput
+
+# >>>>>> --- Insert user details to DB --- <<<<<<
+    def insertUser(self, uInput):
+        result = self.db.UserCollections.insert_one(uInput)
+        if (result.acknowledged):
+            print('User successfully added!!!\n \nUser Object ID is: ', str(result.inserted_id)) #Printing user object id
+                
+# >>>>>> --- Display all user details --- <<<<<<
+    def displayAllUser(self):
+        print('--------- Users Available in UserDataDB ---------')
+        uCollections = self.db.UserCollections.find()
+        for allrecord in uCollections:
+            print(allrecord)
+    
+
+# >>>>>> --- Update the user Address --- <<<<<<
+    def updateUser(self, uInput):
+        self.db.UserCollections.update(
+            {
+                'Name': uInput['Name']
+            },
+            {
+                '$set':{
+                    'Address': uInput['Address']
+                }
+
+            }, multi = False
+            )
+        uCollections = self.db.UserCollections.find({'Name': uInput['Name']})
+        for uInput in uCollections:
+            print('Record updated successfully!!', uInput)
+
+# >>>>>> --- Delete User Details --- <<<<<<
+    def deleteUser(self, uInput):
+        self.db.UserCollections.delete_one({
+            'Name': uInput['Name']
+        })
+        print('!!!-----Record Deleted----!!!')
---
+++ UserDataCollections_working.py
@@ -0,0 +1,68 @@
+from pymongo import MongoClient
+from AllFunctions import SubClass
+client = MongoClient()
+db = client['UserDataDB'] # Creating new Data base 
+subobj = SubClass(db)
+          
+print("---Welcome to User Data Collections Menu---")
+print("Please select any one of the below option: ")
+print("1. Inser user data")
+print("2. Update existing data")
+print("3. Delete existing record")
+print("4. Exit")
+try:
+    choice = int(input("Please enter your choice: ")) # Accepting input from user
+
+    if (choice == 1):
+        uInput = subobj.userInput()
+        print("You have entered the following details: \n", uInput)     
+    
+        result = subobj.findUser(uInput)
+        
+        if (result == 1):
+            print("User already exist...... Insertion Failed!!!")
+            exit
+        else:
+            subobj.insertUser(uInput)
+            subobj.displayAllUser()
+
+    if (choice == 2):
+        uInput = {}
+        print("Enter the name of the user which you want to update: ")
+        uname = str(input("Enter User Name: "))
+        uInput['Name'] = uname
+        result = subobj.findUser(uInput)
+        if (result != 1):
+            print("User Does not exist......")
+            exit
+        else:
+            # subobj.displayOneUser(uInput)
+            print("Please enter the new address: ")
+            uaddress = str(input('Enter user address: '))
+            uInput['Address'] = uaddress
+            subobj.updateUser(uInput)
+            subobj.displayAllUser()
+
+    if(choice == 3):
+        uInput = {}
+        print("Enter the name of the user which you want to delete: ")
+        uname = str(input("Enter User Name: "))
+        uInput['Name'] = uname
+        result = subobj.findUser(uInput)
+        if (result != 1):
+            print("User Does not exist......")
+            exit
+        else:
+            subobj.deleteUser(uInput)
+            subobj.displayAllUser()
+            
+    if (choice == 4):
+        exit
+    if (choice != 1 and choice != 2 and choice != 3 and choice != 4):
+        print("Invalid Input!!! Please enter a number between 1 to 4")
+except ValueError:
+    print("Invalid Input!!! Strings are not allowed")
+except EOFError:
+    print("An error occured while connecting DB")      
+# finally:
+#     print('An error occured while running the program please try again!')  
